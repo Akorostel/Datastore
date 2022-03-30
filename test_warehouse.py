fe51aec180aaeb5e76bcb954e4a6daf5089bedf5
Akorostel <akorostel@gmail.com>
2022-03-30 10:02:55 +0300

    Numerous formatting changes

--- test_warehouse.py
+++ test_warehouse.py
@@ -1,29 +1,86 @@
 """ Warehouse tests """
 
-import unittest
-import warehouse as wh
-from pathlib import Path
 import shutil
+import unittest
+from pathlib import Path
 
-wh_folder = '..\\test_store\\'
+import numpy as np
+import pandas as pd
 
-class TestWarehouse(unittest.TestCase):
+import warehouse as wh
 
+wh_folder = "..\\test_store\\"
+
+
+class TestCreateWarehouse(unittest.TestCase):
     def test_create_no_folder(self):
-        '''Create new warehouse, folder doesnt exist'''
+        """Create new warehouse, folder doesnt exist"""
         p = Path(wh_folder)
         if p.is_dir():
-            print('Deleting old folder...')
+            print("Deleting old folder...")
             shutil.rmtree(wh_folder)
-        with self.assertRaises(ValueError):
+        with self.assertRaises(
+            ValueError, msg="Didn't raise error with non-existing folder"
+        ):
             store = wh.Store(wh_folder)
-    
-#dti = pd.DatetimeIndex(['2020-03-01 00:00:00', '2020-03-01 00:10:00',
-#               '2020-03-01 00:20:00', '2020-03-01 00:30:00'], name='Timestamp')
-#sample_df = pd.DataFrame({'Tag1': [0, 1, 2, 3],
-#                          'Tag2': [0, 10, 20, 30]},
-#                         index=dti)
-    
 
-if __name__ == '__main__':
+    def test_create_empty_folder(self):
+        p = Path(wh_folder)
+        if p.is_dir():
+            print("Deleting old folder...")
+            shutil.rmtree(wh_folder)
+        p.mkdir()
+        store = wh.Store(wh_folder)
+        self.assertTrue(store.vcb.empty, "Returned non-empty vcb just after creation")
+
+    def test_create_folder_with_existing_parquet_files(self):
+        pass
+
+    def test_create_folder_with_existing_other_files(self):
+        pass
+
+
+class TestReadWrite(unittest.TestCase):
+    def setUp(self) -> None:
+        p = Path(wh_folder)
+        if p.is_dir():
+            print("Deleting old folder...")
+            shutil.rmtree(wh_folder)
+        p.mkdir()
+        store = wh.Store(wh_folder)
+
+        nrows = 10
+        dti = pd.date_range(
+            start='2020-03-01 00:00:00',
+            freq='10min',
+            periods=nrows,
+            name='Timestamp'
+        )
+        self.sample_df = pd.DataFrame(np.random.randn(nrows, 4), columns=list("ABCD"), index=dti)
+        store.write(self.sample_df)
+        return super().setUp()
+
+    def test_read_existing(self):
+        store = wh.Store(wh_folder)
+        df = store.read(list("ABCD"))
+        print(self.sample_df)
+        print(df)
+        self.assertTrue(self.sample_df.equals(df))
+        pass
+
+    def test_write_read_random(self):
+        pass
+
+    def test_write_read_csv(self):
+        pass
+
+    def test_many_writes_single_read(self):
+        pass
+
+    def test_single_write_many_reads(self):
+        pass
+
+
+if __name__ == "__main__":
+    print('started')
     unittest.main()
--- warehouse.py
+++ warehouse.py
@@ -1,4 +1,4 @@
-""" 
+''' 
 Simple columnar storage for time-series implemented as a folder of parquet files.
 Designed for fast frequent reads and slow rare writes.
 
@@ -27,21 +27,22 @@ df = store.read(taglist=['2101FIC201.PV', '2101FIC205.PV'],
                 end='2021-03-28 00:00:00')
 # Write to warehouse
 store.write(df)
-"""
+'''
 
-import numpy as np
-import pandas as pd
-import os
-from typing import Dict, List
-from enum import Enum
-from pathlib import Path
-from fastparquet import ParquetFile
-import warnings
-import uuid
-import openpyxl
 import logging
 import logging.handlers
+import os
 import sys
+import uuid
+import warnings
+from enum import Enum
+from pathlib import Path
+from typing import Dict, List
+
+import numpy as np
+import openpyxl
+import pandas as pd
+from fastparquet import ParquetFile
 
 # Setup logging
 logger = logging.getLogger(__name__)
@@ -50,9 +51,10 @@ fh = logging.handlers.TimedRotatingFileHandler('warehouse.log', when='D', backup
 fh.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
-if (logger.hasHandlers()):
+if logger.hasHandlers():
     logger.handlers.clear()
 logger.addHandler(fh)
+
 
 class Store:
     ''' Store object represents a folder which contains a number of parquet files'''
@@ -70,7 +72,7 @@ class Store:
     @property
     def vcb(self):
         return self._vcb
-        
+
     @property
     def path(self):
         return self._path
@@ -94,13 +96,16 @@ class Store:
         elif value > 1000:
             value = 1000
             warnings.warn('tags_per_file set to 1000.')
-        self._tags_per_file = value    
+        self._tags_per_file = value
 
     def _build_vcb(self):
-        """Rebuild vcb dataframe (tag <-> file mapping).
-        Function returns resulting dataframe, but doesn't change self.vcb property """
-        files = [f for f in os.scandir(self._path) if f.is_file() and 
-                                                      'metadata' not in f.name]
+        '''Rebuild vcb dataframe (tag <-> file mapping).
+        Function returns resulting dataframe, but doesn't change self.vcb property '''
+        files = [
+            f
+            for f in os.scandir(self._path)
+            if f.is_file() and 'metadata' not in f.name
+        ]
         tagname_list = []
         filename_list = []
         rows_list = []
@@ -121,14 +126,14 @@ class Store:
                 warnings.warn(f'{f.name} doesnt contain Timestamp column')
                 continue
             rows = pf.info['rows']
-            
+
             try:
                 begin = str(pf.statistics['min']['Timestamp'][0])[:16]
                 end = str(pf.statistics['max']['Timestamp'][0])[:16]
             except:
                 logging.error(f'Unexpected error in rebuild_vcb: {sys.exc_info()[0]}')
                 begin, end = '', ''
-            
+
             for c in columns:
                 tagname_list.append(c)
                 filename_list.append(f.name)
@@ -137,31 +142,39 @@ class Store:
                 end_list.append(end)
 
         if tagname_list:
-            vcb = pd.DataFrame({'Filename': filename_list, 
-                                  'Rows': rows_list,
-                                  'Begin': begin_list,
-                                  'End': end_list}, 
-                                  index=pd.Series(tagname_list, name='TagName')).sort_index()
+            vcb = pd.DataFrame(
+                {
+                    'Filename': filename_list,
+                    'Rows': rows_list,
+                    'Begin': begin_list,
+                    'End': end_list,
+                },
+                index=pd.Series(tagname_list, name='TagName'),
+            ).sort_index()
         else:
-            vcb = pd.DataFrame({'Filename': [], 'Rows': [], 'Begin': [], 'End': []}, 
-                                     index=pd.Series([], name='TagName', dtype='object'))
+            vcb = pd.DataFrame(
+                {'Filename': [], 'Rows': [], 'Begin': [], 'End': []},
+                index=pd.Series([], name='TagName', dtype='object'),
+            )
         return vcb
 
     def rebuild_vcb(self):
-        """Rebuild vcb dataframe (tag <-> file mapping). """
+        '''Rebuild vcb dataframe (tag <-> file mapping). '''
         self._vcb = self._build_vcb()
         return self._vcb
-    
-    #TODO: strange warning from pandas
+
+    # TODO: strange warning from pandas
     # A value is trying to be set on a copy of a slice from a DataFrame.
     # Try using .loc[row_indexer,col_indexer] = value instead
     # Where is it coming from?
-    
+
     def _update_file(self, dataframe: pd.DataFrame, filename: str):
         p = Path(self.path).joinpath(filename)
         file_content = pd.read_parquet(p)
 
-        new_before, new_after, new_overlaps, inconsistent_freq = check_time_range(file_content.index, dataframe.index)
+        new_before, new_after, new_overlaps, inconsistent_freq = check_time_range(
+            file_content.index, dataframe.index
+        )
         if new_overlaps:
             msg = f'While updating file {filename} with tags {dataframe.columns.tolist()}, timestamp overlap was detected.'
             logger.warning(msg)
@@ -174,118 +187,162 @@ class Store:
         updated_frame = dataframe.combine_first(file_content)
         updated_frame.sort_index(inplace=True)
         updated_frame.to_parquet(p)
-        logger.info(f'Tags {dataframe.columns.tolist()} written to existing file {filename}')
+        logger.info(
+            f'Tags {dataframe.columns.tolist()} written to existing file {filename}'
+        )
 
     def write(self, dataframe: pd.DataFrame, new_vcb: pd.DataFrame = None):
         '''Write dataframe tags to the store.
         Tag<->file mapping will be done automatically, but can also
         be provided in new_vcb dataframe (not supported yet)'''
-        #TODO: support for new_vcb
-        
+        # TODO: support for new_vcb
+
+		# TODO: think about the following situation:
+		# first we write 1M data 00:05:00, 00:06:00, 00:07:00
+		# then we write 1M data 00:05:10, 00:06:10, 00:07:10
+		# when reading combined result it contains all timestamps 00:05:00, 00:05:10, 00:06:00, 00:06:10
+		# and some values are missing (filled with NaN).
+		
         if not dataframe_is_valid(dataframe):
             raise ValueError('Please check if dataframe is valid.')
 
         # tags in dataframe:
         #   some already exist in the warehouse
-        #       if file location is specified for existing tag and does not equal to its real location, then generate warning and exit
+        #       if file location is specified for existing tag and does not equal to its real 
+        #            location, then generate warning and exit
         #       otherwise save the tag to existing file location
         #   some are new
         #       if file location is specified, use it
         #       if not specified - generate new file for them
-        #           or try to place new tags in existing files which have free space (this will be implemented later)
-        
+        #           or try to place new tags in existing files which have free space 
+        #           (this may be implemented later)
+
         # 1. Generate tag->file mapping: tag, file, existing
         tags_to_write = pd.Series(True, index=dataframe.columns, name='Write')
-        vcb_tags_to_write = self._vcb.join(tags_to_write, how='right').sort_values(by='Filename')
+        vcb_tags_to_write = self._vcb.join(tags_to_write, how='right').sort_values(
+            by='Filename'
+        )
         vcb_tags_to_write['Existing'] = ~vcb_tags_to_write['Filename'].isna()
 
-        existing_files = vcb_tags_to_write[vcb_tags_to_write['Existing']]['Filename'].unique().tolist()
+        existing_files = (
+            vcb_tags_to_write[vcb_tags_to_write['Existing']]['Filename']
+            .unique()
+            .tolist()
+        )
         unmapped_tags = vcb_tags_to_write[~vcb_tags_to_write['Existing']].index.tolist()
         # 2. For each existing file,
         #       Call update_file(file, dataframe)
         for f in existing_files:
-            tags = vcb_tags_to_write[vcb_tags_to_write['Filename']==f].index.tolist()
+            tags = vcb_tags_to_write[vcb_tags_to_write['Filename'] == f].index.tolist()
             self._update_file(dataframe[tags], f)
 
         # 3. For each non-existing file,
         #       Call dataframe.to_parquet()
         tags_per_file = 10
-        tag_groups = [unmapped_tags[i:i + tags_per_file] for i in range(0, len(unmapped_tags), tags_per_file)]
+        tag_groups = [
+            unmapped_tags[i : i + tags_per_file]
+            for i in range(0, len(unmapped_tags), tags_per_file)
+        ]
         for tags in tag_groups:
-            f = Path(self._path).joinpath(uuid.uuid4().hex + '.parquet') # unique filename
+            f = Path(self._path).joinpath(
+                uuid.uuid4().hex + '.parquet'
+            )  # unique filename
+			# TODO: check if whole tag_group is empty (contains no data). This will result in wrong statistics in parquet file
             dataframe[tags].to_parquet(f)
             logger.info(f'Tags {tags} written to newly created file {f}')
 
         # Maybe its better to update only new/changed entries in vcb, rather than make full rebuild
         self.rebuild_vcb()
 
-    def read(self, tags: List[str], begin: str=None, end: str=None) -> pd.DataFrame:
+    def read(self, tags: List[str], begin: str = None, end: str = None) -> pd.DataFrame:
         '''Reads specified tags from the store'''
-        
+
         tags_to_read = pd.Series(True, index=tags, name='Read')
-        vcb_tags_to_read = self._vcb.join(tags_to_read, how='right').sort_values(by='Filename')
+        vcb_tags_to_read = self._vcb.join(tags_to_read, how='right').sort_values(
+            by='Filename'
+        )
         vcb_tags_to_read['Existing'] = ~vcb_tags_to_read['Filename'].isna()
 
-        existing_files = vcb_tags_to_read[vcb_tags_to_read['Existing']]['Filename'].unique().tolist()
+        existing_files = (
+            vcb_tags_to_read[vcb_tags_to_read['Existing']]['Filename'].unique().tolist()
+        )
         unmapped_tags = vcb_tags_to_read[~vcb_tags_to_read['Existing']].index.tolist()
-        if len(unmapped_tags)>0:
+        if len(unmapped_tags) > 0:
             raise KeyError('No such tag in the store: ' + ', '.join(unmapped_tags))
 
         list_of_dataframes = []
         for f in existing_files:
-            file_tags = vcb_tags_to_read[vcb_tags_to_read['Filename']==f].index.tolist()
-            list_of_dataframes.append(pd.read_parquet(Path(self._path).joinpath(f), columns=file_tags))
+            file_tags = vcb_tags_to_read[
+                vcb_tags_to_read['Filename'] == f
+            ].index.tolist()
+            list_of_dataframes.append(
+                pd.read_parquet(Path(self._path).joinpath(f), columns=file_tags)
+            )
             logger.debug(f'Read from file {f}. Tags: {file_tags}')
         d = pd.concat(list_of_dataframes, axis=1, join='outer', sort=True)
         if begin:
             d = d.loc[begin:]
         if end:
             d = d.loc[:end]
-        #TODO: do we need resample? Is it possible to have different files in the store with
+        # TODO: do we need resample? Is it possible to have different files in the store with
         # different samplerates?
         return d[tags]
 
-    def update_from_excel_file(self, xl_path: str, resample_interval:str=None):
-        #TODO: add timing
+    def update_from_excel_file(self, xl_path: str, resample_interval: str = None):
+        # TODO: add timing
 
         logger.info(f'Reading {xl_path}')
-        #TODO: check if index column is called 'Date'
-        odct = pd.read_excel(xl_path, 
-                             sheet_name=None,
-                             index_col='Date')
+        # TODO: check if index column is called 'Date'
+		# TODO: check for empty columns 
+        odct = pd.read_excel(xl_path, sheet_name=None, index_col='Date')
         for sheetname, d in odct.items():
             d.index.name = 'Timestamp'
             if 'Time' in d.columns:
-                d.drop('Time',axis=1, inplace=True)
+                d.drop('Time', axis=1, inplace=True)
             # remove duplicated records (May appear when switching to daylight saving time)
             is_duplicated = d.index.duplicated()
-            n_duplicated = is_duplicated.sum() # default keep='First', so old values are preserved
+            n_duplicated = (
+                is_duplicated.sum()
+            )  # default keep='First', so old values are preserved
             if n_duplicated:
-                logger.warning(f'While reading file {xl_path}, sheet {sheetname}, {n_duplicated} duplicated records were found.')
+                logger.warning(
+                    f'While reading file {xl_path}, sheet {sheetname}, {n_duplicated} duplicated records were found.'
+                )
             d = d.loc[~is_duplicated]
             # convert int columns to float
-            cols_to_float = (d.dtypes==np.int64)
-            d.loc[:,cols_to_float]=d.loc[:,cols_to_float].astype('float64')
+            cols_to_float = d.dtypes == np.int64
+            d.loc[:, cols_to_float] = d.loc[:, cols_to_float].astype('float64')
             if resample_interval:
                 d = d.resample(resample_interval).ffill()
             self.write(d)
 
         return
 
+    def delete(self, tags: List[str]):
+        '''Delete specified tags from the store'''
+		#TODO
+        return
+
     def reorganize(self, new_path: str, new_vcb: pd.DataFrame):
         '''Move data to new storage with given tag<->file mapping'''
-        #TODO:
+        # TODO:
         pass
-    
+
+
 # TODO: Do we need it here? Maybe ROW_WISE storage is easyly implemented with single sqlite table?
 # ok, but we need 'update from excel' operation...
-#class StoreType(Enum):
+# class StoreType(Enum):
 #    COLUMN_WISE = 1
 #    ROW_WISE = 2
 
 
 def dataframe_is_valid(dataframe: pd.DataFrame):
     '''Check if dataframe can be written to the store'''
+
+    # must be a dataframe
+    if not isinstance(dataframe, pd.DataFrame):
+        print('Passed object is not a dataframe.')
+        return False
 
     # must have datetime index
     if not isinstance(dataframe.index, pd.DatetimeIndex):
@@ -304,11 +361,12 @@ def dataframe_is_valid(dataframe: pd.DataFrame):
 
     return True
 
+
 def get_freq(dti: pd.DatetimeIndex):
-    """Returns frequency of the index:
+    '''Returns frequency of the index:
     index.freq if it is specified,
     index.inferred_freq if it can be computed,
-    If it cannot be computed, tries to infer based on last 5 elements."""
+    If it cannot be computed, tries to infer based on last 5 elements.'''
 
     if dti.freq:
         return dti.freq
@@ -317,18 +375,19 @@ def get_freq(dti: pd.DatetimeIndex):
     else:
         return dti[-5:].inferred_freq
 
+
 def check_time_range(existing_index, new_index):
     exist_begin = existing_index[0]
     exist_end = existing_index[-1]
     new_begin = new_index[0]
     new_end = new_index[-1]
-    
+
     if exist_begin > exist_end:
         raise ValueError('Begin of existing index is later then its end')
     if new_begin > new_end:
         raise ValueError('Begin of new index is later then its end')
 
-    new_before, new_after, new_overlaps = False, False, False   
+    new_before, new_after, new_overlaps = False, False, False
 
     if new_begin > exist_end:
         new_after = True
@@ -343,10 +402,11 @@ def check_time_range(existing_index, new_index):
 
     return new_before, new_after, new_overlaps, inconsistent_freq
 
+
 def get_xl_info(xl_path: str) -> Dict:
     wb = openpyxl.load_workbook(xl_path, read_only=True)
     xl_info = dict()
     for sh in wb.worksheets:
-        sh.title # get max_col, max_row takes too much time
+        sh.title  # get max_col, max_row takes too much time
 
     return xl_info
